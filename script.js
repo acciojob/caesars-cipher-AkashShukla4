e9a0a98f0f9fdc1abaa23ae50570213562c67f11
Akash Shukla <117896950+AkashShukla4@users.noreply.github.com>
2023-06-26 11:48:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+   let decodedArr = []; 
 	var words = encodedStr.split(" ");
 	for(let i = 0; i < words.length; i++){
 		var eword = words[i];
